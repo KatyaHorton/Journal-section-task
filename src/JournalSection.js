707e7e352468ce5cb9307890f00c0d956c856b65
Katya Horton <ekaterina.horton@gmail.com>
2018-10-15 10:35:45 +0100

    feat: Map through data to display articles info

--- src/JournalSection.js
+++ src/JournalSection.js
@@ -5,9 +5,30 @@ class JournalSection extends Component {
 	
   render() {
 	  
+	  console.log(Data.articles)
+	  
     return (
       <div className="journal-section">
-			Journal Section Component 
+		
+		{Data.articles.map((article, index) => {
+		
+		return <div>
+			<div className='time-container'>
+				{article.data_time}
+			</div>
+			<div className='image-container'>
+			</div>
+			<div className='article-number'>
+				{index+1}
+			</div>
+			<div className='text-container'>
+				<h1>{article.title}</h1>
+				<p>{article.body}</p>
+				<a href={article.url}>LINK</a>
+			</div>
+			</div>
+		
+	})}
       </div>
     );
   }
