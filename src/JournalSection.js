847d39a628220be2f3329ec9a57324cce9de3457
Katya Horton <ekaterina.horton@gmail.com>
2018-10-15 09:42:21 +0100

    feat: Create JournalSection component

---
+++ src/JournalSection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class JournalSection extends Component {
+  render() {
+    return (
+      <div className="journal-section">
+			Journal Section Component 
+      </div>
+    );
+  }
+}
+
+export default JournalSection
