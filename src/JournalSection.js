bb204378a28563f12da8fb986a8d8c9738e07357
Katya Horton <ekaterina.horton@gmail.com>
2018-10-15 12:05:44 +0100

    feat: Add suffix to a 'day'

--- src/JournalSection.js
+++ src/JournalSection.js
@@ -11,28 +11,49 @@ class JournalSection extends Component {
 		const answer_array = date.split('-');
 		const year = answer_array[0]
 		const month = answer_array[1]
+		
+// assign day a value and add a suffix to a number 
 		const day = answer_array[2].split('T')[0]
+		const daySuffix = this.addSuffix(day)
 		console.log('splited_array: ', answer_array)
 		console.log('year: ', year)
 		console.log('month: ', month)
-		console.log('day: ', day)
+		console.log('day: ', this.addSuffix(day))
 		return (
 		<div className='article-date'>
 			<div className='article-day'>
-				{day}
+				{daySuffix}
 			</div>
 			<div className='article-month-year'>
 				{month} {year}
 			</div>
-			
 		</div>
 		
 		)
 	}
 	
+	
+// Function to add suffix to a DD
+	
+addSuffix(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+	
   render() {
 	  
 	  console.log(Data.articles)
+	  
 	  
     return (
       <div className="journal-section">
